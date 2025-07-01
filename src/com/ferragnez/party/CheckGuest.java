0fe61aa2d543720b7a5ed0614cfd77f14fd1e429
vmqxdv <vquintilio@gmail.com>
2025-07-01 19:19:08 +0200

    feat: Implement guest check functionality with for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,6 +1,13 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
+
+  static String formatStringForCheck(String str) {
+    return str.toLowerCase().replaceAll("\\s+", "");
+  }
+
   public static void main(String[] args) {
     String[] guestsArr = {
         "Dua Lipa",
@@ -16,5 +23,27 @@ public class CheckGuest {
         "Rachel Zeilic"
     };
 
+    System.out.println("Benvenuto alla festa dei Ferragnez! Per favore forniscimi il tuo nome:");
+
+    Scanner in = new Scanner(System.in);
+
+    String userName = in.nextLine();
+
+    boolean isUserFound = false;
+
+    // Soluzione con for
+    for (int i = 0; i < guestsArr.length; i++) {
+      if (formatStringForCheck(guestsArr[i]).equals(formatStringForCheck(userName))) {
+        isUserFound = true;
+        break;
+      }
+    }
+
+    if (isUserFound) {
+      System.out.println("Oh eccoti qua, puoi entrare.");
+    } else
+      System.out.println("Spiacente, non sei sulla lista. Non puoi entrare.");
+
+    in.close();
   }
 }
